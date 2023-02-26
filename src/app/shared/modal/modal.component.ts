9938562b11613d22b27d694deecc3b3ee72a4abb
Jonathon Ireland <jonathon.ireland@dcsg.com>
2023-02-26 10:56:42 -0500

    At working state Lecture 87

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { ModalService } from 'src/app/services/modal.service';
 
@@ -9,13 +9,15 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 export class ModalComponent implements OnInit {
 
+  @Input() modalID =``;
+
   constructor(public modal: ModalService ) {}
 
   ngOnInit(): void {
   }
 
   closeModal(){
-    this.modal.toggleModal()
+    this.modal.toggleModal(this.modalID)
   }
 
 }
--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ModalService } from 'src/app/services/modal.service';
+
 @Component({
   selector: 'app-auth-modal',
   templateUrl: './auth-modal.component.html',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modal: ModalService) { }
 
   ngOnInit(): void {
+    this.modal.register('auth')
   }
 
 }
