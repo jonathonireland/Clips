fcca270268331d76c1ca282100be46c363b1350e
Jonathon Ireland <jonathon.ireland@dcsg.com>
2023-02-26 13:10:25 -0500

    tabs and tabContainer lectures 91 and 92

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,23 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { ModalService } from '../services/modal.service';
 import { NgModule } from '@angular/core';
+import { TabComponent } from './tab/tab.component';
+import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 // import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
-    ModalComponent
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    ModalComponent
+    ModalComponent,
+    TabComponent,
+    TabsContainerComponent
   ],
   providers: [
     ModalService
---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,21 @@
+import { AfterContentInit, Component, ContentChildren } from '@angular/core';
+
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.scss']
+})
+export class TabsContainerComponent implements AfterContentInit {
+
+  @ContentChildren(TabComponent) tabs = {}
+
+  constructor() { }
+
+  ngAfterContentInit(): void {
+    console.log(this.tabs);
+  }
+
+ 
+}
