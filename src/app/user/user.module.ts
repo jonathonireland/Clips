7422aff8f262ee1bc2e7053f4e54a6af405c4d02
Jonathon Ireland <jonathon.ireland@dcsg.com>
2023-02-26 13:49:30 -0500

    Preparing the Forms Lecture 98

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,14 @@ import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AuthModalComponent
+    AuthModalComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
