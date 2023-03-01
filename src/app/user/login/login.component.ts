02f41162ccdc14fb41b1846a8c56a0f7fc108faa
Jonathon Ireland <jonathon.ireland@dcsg.com>
2023-03-01 17:16:35 -0500

    Lect 120 Two-way Binding

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  credentials = {
+    email: '',
+    password: ''
+  }
 
   constructor() { }
 
