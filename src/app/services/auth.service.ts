4aa1f022ba8eacfb7f3097ec85cc248deb1c0976
Jonathon Ireland <jonathon.ireland@dcsg.com>
2023-03-03 17:26:55 -0500

    Lect 161 Collection Types and 162 Connecting User with their data

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
 import IUser from 'src/app/models/user.model';
 import { Injectable } from '@angular/core';
 
@@ -7,14 +8,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+  private usersCollection: AngularFirestoreCollection<IUser>
 
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) { }
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
+    this.usersCollection = db.collection('users');
+   }
 
   public async createUser(userData: IUser) {
+    if(!userData.password){
+      throw new Error("Password not provided!")
+    }
     const userCred = await this.auth.createUserWithEmailAndPassword(
       userData.email, userData.password
     )
-    await this.db.collection('users').add({
+    await this.usersCollection.add({
       name: userData.name,
       age: userData.age,
       phoneNumber: userData.phoneNumber,
