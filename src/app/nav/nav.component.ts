fe4b5915054fac0149c6e27c0ca99135e952f1d3
Jonathon Ireland <jonathon.ireland@dcsg.com>
2023-03-05 11:59:52 -0500

    Lect 169-172 Login and Signing Out

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
 
@@ -12,7 +13,8 @@ export class NavComponent implements OnInit {
 
   constructor(
     public modal: ModalService, 
-    public auth: AuthService
+    public auth: AuthService,
+    public afAuth: AngularFireAuth
     ) { }
 
   ngOnInit(): void {
@@ -21,6 +23,11 @@ export class NavComponent implements OnInit {
   openModal($event: Event){
     $event.preventDefault();
     this.modal.toggleModal('auth');
+  }
+
+  async logout($event: Event){
+    $event.preventDefault()
+    await this.afAuth.signOut();
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,10 @@
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { delay, map } from 'rxjs/operators';
 
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import IUser from 'src/app/models/user.model';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +12,16 @@ import { map } from 'rxjs';
 export class AuthService {
   private usersCollection: AngularFirestoreCollection<IUser>;
   public isAuthenticated$: Observable<boolean>;
+  public isAuthenticatedWithDelay$: Observable<boolean>;
 
   constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
     this.usersCollection = db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(
       map(user => !!user )
     );
+    this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
+      delay(1000)
+    )
    }
 
   public async createUser(userData: IUser) {
