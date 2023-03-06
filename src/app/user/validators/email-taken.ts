c852964db03427f696a1383c660e1bb46d72aa1b
Jonathon Ireland <jonathon.ireland@dcsg.com>
2023-03-06 18:20:42 -0500

    Lect 179-180 Validating Unique Emails.

---
+++ src/app/user/validators/email-taken.ts
@@ -0,0 +1,15 @@
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class EmailTaken implements AsyncValidator {
+    constructor(private auth: AngularFireAuth){ }
+    
+    validate(control: AbstractControl) : Promise<ValidationErrors | null > {
+       return this.auth.fetchSignInMethodsForEmail(control.value).then(
+            response => response.length ? { emailTaken: true } : null
+        )
+    }
+}
