89766c0bdd2f91b6adb80393d74c228c3ae91592
Jonathon Ireland <jonathon.ireland@dcsg.com>
2023-03-15 00:26:23 -0400

    Lect 202 Route Guards

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -1,23 +1,30 @@
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ManageComponent } from './manage/manage.component';
 import { NgModule } from '@angular/core';
 import { UploadComponent } from './upload/upload.component';
 
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/')
+
 const routes: Routes = [
 	{
 		path: 'manage',
 		component: ManageComponent,
 		data: {
-			authOnly: true
-		}
+			authOnly: true,
+			authGuardPipe: redirectUnauthorizedToHome
+		},
+		canActivate: [AngularFireAuthGuard]
 	},
 	{
 		path: 'upload',
 		component: UploadComponent,
 		data: {
-			authOnly: true
-		}
+			authOnly: true,
+			authGuardPipe: redirectUnauthorizedToHome
+		},
+		canActivate: [AngularFireAuthGuard]
 	},
 	{
 		path: 'manage-clips',
