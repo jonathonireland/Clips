0e58461ec54c914a26c4d6383cbba417f62b3fd0
Jonathon Ireland <jonathon.ireland@dcsg.com>
2023-03-14 23:40:31 -0400

    Lect 196 Subscribing to Route Parameters

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,24 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./manage.component.scss']
 })
 export class ManageComponent implements OnInit {
+	videoOrder = '1'
+	
+	constructor(
+		private router: Router,
+		private route: ActivatedRoute,
+	){}
 	
 	ngOnInit(): void {
+		this.route.queryParamMap.subscribe((params: Params)=>{
+			console.log(params['get'])
+			this.videoOrder = params['get']('sort') === '2' ? params['get']('sort') : '1'
+		})
+	}
+ 
+	sort(event: Event) {
+		const { value } = (event.target as HTMLSelectElement)
+
+		this.router.navigateByUrl(`/manage?sort=${value}`)
 	}
 
 }
